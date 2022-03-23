8d7271953b4513c7f3adefdcb2a0c9bc4788aebb
Your Name <you@example.com>
2022-03-23 14:38:13 -0700

    add icon

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faStar } from "@fortawesome/free-solid-svg-icons";
+import "./Product.css";
 const Product = (props) => {
-    const {img,name,price,ratings}=props?.product;
-    
-  return (
-    <div className='product-container'>
-    <img src={img} alt="" />
-    <div className="product-detail">
-    <p className='product-name'>{name}</p>
-    <p className='product-price'>Price: ${price}</p>
-    <p><small>Rating: {ratings}</small></p>
-    </div>
-    
-    
-    <button className='product-btn' onClick={()=>props.handleAddToCart(props.product)}>Add to Cart</button>
-   
-    </div>
-  )
-}
+  const { img, name, price, ratings } = props?.product;
 
-export default Product
+  return (
+    <div className="product-container">
+      <img src={img} alt="" />
+      <div className="product-detail">
+        <p className="product-name">{name}</p>
+        <p className="product-price">Price: ${price}</p>
+        <p>
+          <small>Rating: {ratings} <FontAwesomeIcon icon={faStar} /></small>
+        </p>
+      </div>
+
+      <button
+        className="product-btn"
+        onClick={() => props.handleAddToCart(props.product)}
+      >
+        Add to Cart <FontAwesomeIcon icon={faCartPlus} />
+      </button>
+    </div>
+  );
+};
+
+export default Product;
